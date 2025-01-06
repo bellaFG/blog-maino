0fb40c431a5c4f7e203b49f8bb9a938964974a42
bellaFG <isabella.ferzales@gmail.com>
2025-01-06 12:01:21 -0300

    Configurações finais para o database

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ bundle exec rails assets:clean
 # Se você estiver usando um tipo de instância Free, você precisa
 # realizar migrações de banco de dados no comando de construção.
 # Remova o comentário da seguinte linha:
- bundle exec rails db:migrate
+bundle exec rails db:migrate
