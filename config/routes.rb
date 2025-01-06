4bf0b9d73b56497fe0242510cda8ed8151d31916
bellaFG <isabella.ferzales@gmail.com>
2025-01-06 19:03:34 -0300

    Adicionar friendly_id aos artigos e atualizar views >>

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  # Configuração do Devise
   devise_for :users, controllers: { sessions: "users/sessions" }
 
-  # Rota principal (root)
+
   root "articles#index"
 
-  # Rotas para Articles e Comments
   resources :articles, param: :slug do
     collection do
-      get :search, to: "articles#search" # Rota para busca de artigos
+      get :search, to: "articles#search"
     end
 
-    resources :comments, only: [ :create ] # Rotas para comentários associados a artigos
+    resources :comments, only: [ :create ]
   end
 end
