6557628314a02e309af04bdebaef1ad3b02d3193
bellaFG <isabella.ferzales@gmail.com>
2025-01-06 20:32:04 -0300

    Adiciona funcionalidades de login, cadastro e logout com Devise

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "bootstrap"
-import Rails from "@rails/ujs";
-Rails.start();
-
+import Rails from "@rails/ujs"
+Rails.start()
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 pin "application"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-pin "rails-ujs", to: "https://cdn.jsdelivr.net/npm/@rails/ujs@7.0.0-beta1/dist/rails-ujs.min.js"
+pin "@rails/ujs", to: "https://cdn.jsdelivr.net/npm/@rails/ujs@7.0.0-beta1/dist/rails-ujs.min.js"
+pin "turbo", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo-rails@7.0.0-beta.1/dist/turbo.min.js"
